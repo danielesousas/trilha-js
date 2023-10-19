59d9d59d3d5c1e9dea19f02c98c2a9288b16f211
danielesousas <145569612+danielesousas@users.noreply.github.com>
2023-10-19 00:00:37 -0300

    update

---
+++ ex14.js
@@ -0,0 +1,9 @@
+var nomeEntrada = 'ivi';
+
+var palavraGerada = combinandoNomesPokemons(nomeEntrada);
+
+
+function combinandoNomesPokemons() {
+  console.log(nomeEntrada + 'saur')
+}
+// Exib
---
+++ ex15.js
@@ -0,0 +1,29 @@
+
+
+// Solicita ao usuário o tipo de magia
+const tipoMagia = gets('ataque');
+
+// Solicita ao usuário a quantidade de vezes que a magia será usada
+const quantidade = parseInt(gets(1));
+
+// Obtem a mensagem correspondente ao tipo de magia
+const mensagem = obterMensagem(tipoMagia);
+
+
+//TODO: Crie uma função ou outra estrutura condicional para retornar a mensagem correspondente ao tipo de magia:
+function   obterMensagem(tipoMagia){
+
+// Criamos um objeto 'mensagens' para mapear os tipos de magia para mensagens correspondentes.
+  const mensagens = {
+    ataque: "Usou magia de ataque!",
+    cura: "Usou magia de cura!",
+    defesa: "Usou magia de defesa!",
+    invalido: "Tipo de magia inválido!"
+  };
+// Aqui fica o retorno da função com a mensagem associada ao tipo de magia fornecido:
+  return mensagens[tipoMagia] || mensagens.invalido;
+}
+// É impresso a mensagem a quantidade de vezes especificada:
+for (let i = 0; i < quantidade; i++) {
+print(mensagem);
+}
